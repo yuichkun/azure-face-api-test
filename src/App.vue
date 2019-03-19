288a3f8f52007fa421132cf5f2f7ce5216cf71a5
Yuichi Yogo <yuichkun@gmail.com>
2019-03-19 21:24:47 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// NOTE: You must use the same region in your REST call as you used to
+// obtain your subscription keys. For example, if you obtained your
+// subscription keys from westus, replace "westcentralus" in the URL
+// below with "westus".
+//
+// Free trial subscription keys are generated in the "westus" region.
+// If you use a free trial subscription key, you shouldn't need to change
+// this region.
+
+const uriBase =
+  "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+// Request parameters.
+const params = {
+  returnFaceId: "true",
+  returnFaceLandmarks: "false",
+  returnFaceAttributes:
+    "age,gender,headPose,smile,facialHair,glasses,emotion," +
+    "hair,makeup,occlusion,accessories,blur,exposure,noise"
+};
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  components: {}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
